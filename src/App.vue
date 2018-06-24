70a753bcb834d10a14de89151579cfc171b8929a
chien <sarahxoxo@users.noreply.github.com>
2018-06-25 02:15:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <header class="navbar">
+      <div class="logo col-md-2">Shoptime</div>
+      <div class="tabs d-flex col-md-10">
+        <div class="tab">HOME</div>
+        <div class="tab">ORDERS</div>
+        <div class="tab">PRODUCT</div>
+      </div>
+    </header>
+    <div class="body">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+@import "bootstrap/dist/css/bootstrap.min.css"
+</style>
+<style>
+* {
+  font-family: HelveticaNeue;
+}
+:root {
+  --red-color: #D0021B;
+  --yellow-color: #F5A623;
+  --blue-color: #4A90E2;
+  --green-color: #7ED321;
+  --dark-gray: #9B9B9B;
+  --background-color: #F2F2F2;
+}
+</style>
+<style scoped>
+.navbar {
+  background: #000;
+  color: #fff;
+  font-weight: bold;
+}
+.logo {
+  font-size: 24px;
+}
+.tab {
+  color: var(--dark-gray);
+  margin: 26px 20px 25px;
+}
+.tab.active {
+  color: #fff;
+}
+.body {
+  background: var(--background-color);
+  padding: 30px 42px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="page-container">
+    <div class="title">OVERVIEW</div>
+    <div class="box-container d-flex">
+      <div class="total-box col-md-4">
+        <div><i class="fas fa-hand-holding-usd"></i>TOTAL REVENUE</div>
+        <div class="num">54,540</div>
+      </div>
+      <div class="total-box col-md-4">
+        <div><i class="fas fa-boxes"></i>TOTAL COST</div>
+        <div class="num">12,660</div>
+      </div>
+      <div class="total-box col-md-4">
+        <div><i class="fas fa-money-bill"></i>NET IMCOME</div>
+        <div class="num">4,1880</div>
+      </div>
+    </div>
+    <div class="box col-md-6">
+      <div class="title">Transaction Website</div>
+      <div class="list-item d-flex">
+        <div class="icon col-md-4"><i class="fab fa-facebook"></i><span>Facebook.com</span></div>
+        <div class="figure col-md-4">45,836</div>
+        <div class="percent col-md-4"><i class="fas fa-arrow-up"></i>20%</div>
+      </div>
+    </div>
+    <div class="box col-md-6">
+      <div class="title">Latest Order</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style scoped>
+.title {
+  font-size: 24px;
+  font-weight: bold;
+}
+.total-box {
+  background: #fff;
+  padding: 26px;
+  text-align: center;
+  margin: 0 20px 18px 0;
+}
+.num {
+  font-size: 36px;
+  font-weight: bold;
+}
+.total-box:first-child .num {
+  color: var(--green-color);
+}
+.total-box:nth-child(2) .num {
+  color: var(--red-color);
+}
+.total-box:nth-child(3) .num {
+  color: var(--blue-color);
+}
+.box {
+  padding: 30px 40px;
+}
+.list-item i {
+  font-size: 45px;
+}
+.list-item span {
+  color: var(--dark-gray);
+}
+.percent {
+  color: var(--green-color);
+}
+.percent i {
+  font-size: 17px;
+}
+</style>
